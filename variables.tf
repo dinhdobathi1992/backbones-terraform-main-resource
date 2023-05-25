a36c8e31006b68d260bfbcd224f25e17db3477fe
Thi <67835317+dinhdobathi1992@users.noreply.github.com>
2023-05-25 22:33:39 +0700

    initial

--- variables.tf
+++ variables.tf
@@ -4,28 +4,28 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "ap-southeast-1"
+  default     = ""
 }
 
 variable "cidr" {
   description = "VPC CIDR"
   type = string
-  default = "10.0.0.0/16"
+  default = ""
 }
 
 variable "vpc_name" {
   description = "VPC Name"
   type = string
-  default = "Dev-vpc"
+  default = ""
 }
 variable "private_subnets" {
   description = "A list of private subnets inside the VPC"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = []
 }
 
 variable "public_subnets" {
   description = "A list of private subnets inside the VPC"
   type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  default     = []
 }
